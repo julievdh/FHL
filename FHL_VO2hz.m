b9db98516b32ce053a6d34ca831cf21ad8c3b958
julievdh <julievdh@mit.edu>
2015-07-02 09:38:50 -0400

    Fit LM on log data

--- FHL_VO2hz.m
+++ FHL_VO2hz.m
@@ -1,58 +1,40 @@
 % Calculate relationship between VO2 and pectoral fin frequency
 
-
-
 % load data
-cd /Users/julievanderhoop/Documents/MATLAB/FHL
+cd /Users/julievanderhoop/Documents/MATLAB/FHL/FinBeats
 load('FHL_hzVO2.mat')
+% same as before: flow == 0 when turbulent, == 1 when laminar
+
+%%
+logVO2 = log(VO2);
+
+Pecs = table(Hz,logVO2,fish,flow,speed);
+Pecs.flow = nominal(Pecs.flow);
+
+f = fitlm(Pecs,'logVO2~flow*Hz');
+figure(2); clf; hold on
+h = gscatter(Hz,logVO2,flow,'br','o^');
+set(h(1),'MarkerFaceColor','b');
+set(h(2),'MarkerFaceColor','r');
+
+w = linspace(min(Hz),max(Hz(flow == 0)));
+line(w,feval(f,w,'0'),'Color','b')
+w = linspace(min(Hz),max(Hz(flow == 1)));
+line(w,feval(f,w,'1'),'Color','r')
+
+anova(f);
+legend('High Turbulence','Low Turbulence','Location','NW')
+xlabel('Pectoral Fin Beat Frequency (Hz)'); ylabel('logVO_2 (mg O_2 kg^-^1 h^-^1)')
+adjustfigurefont; box on
+
+%%
+
+[lamfit,lamgof,lamoutput] = fit(Hz(flow == 1),VO2(flow == 1),'exp1');
+[turbfit,turbgof,turboutput] = fit(Hz(flow == 0),VO2(flow == 0),'exp1');
+figure(1); clf; hold on
+plot(lamfit); 
+h = plot(turbfit); set(h,'color','b')
+h = gscatter(Hz,VO2,flow,'br','o^');
 
 
-% fish and condition and speed
-fish = {'5';'5';'5';'5';'5';'5';...
-    '6';'6';'6';'6';'6';'6';...
-    '9';'9';'9';'9';'9';'9';...
-    '10';'10';'10';'10';'10';'10';...
-    '11';'11';'11';'11';'11';'11';...
-    '13';'13';'13';'13';'13';'13';...
-    '15';'15';'15';'15';'15';'15';...
-    '19';'19';'19';'19';'19';'19'};
-speed = repmat((1:3),1,16)';
-condition = repmat([1 1 1 0 0 0],1,8)';
-
-
-x = 0.5:0.1:3;
-
-[s i stats] = myregr(hzVO2(condition == 0,3),hzVO2(condition == 0,2),0);
-turbfit = s.value*x + i.value;
-
-[s i stats] = myregr(hzVO2(condition == 1,3),hzVO2(condition == 1,2),0);
-lamfit = s.value*x + i.value;
-
-[s i stats] = myregr(hzVO2(:,3),hzVO2(:,2),0);
-allfit = s.value*x + i.value;
-
-
-% plot
-figure(1); clf; hold on; box on
-set(gca,'FontSize',12)
-plot(hzVO2(condition == 0,3),hzVO2(condition == 0,2),'bo','MarkerFaceColor','b')
-plot(hzVO2(condition == 1,3),hzVO2(condition == 1,2),'ro','MarkerFaceColor','r')
-% plot(x,turbfit,'b',x,lamfit,'r',x,allfit,'k')
-plot(x,allfit,'k')
-xlabel('Pectoral Fin Beat Frequency (Hz)','FontSize',12); ylabel('Oxygen Consumption Rate (VO_2 ; mg O_2 kg^-^1 h^-^1)','FontSize',12)
-
-% cd /Users/julievanderhoop/Documents/MATLAB/FHL
-% savename = 'VO2Hz'
-% print('-depsc',savename)
-
-% regstats(hzVO2(:,3),hzVO2(:,2),'linear')
-stepwise(hzVO2(:,3:7),hzVO2(:,2))
-
-figure(2); clf; hold on; box on
-set(gca,'FontSize',12)
-plot(hzVO2(condition == 0,3).*hzVO2(condition == 0,5)/100,hzVO2(condition == 0,2),'bo','MarkerFaceColor','b')
-plot(hzVO2(condition == 1,3).*hzVO2(condition == 1,5)/100,hzVO2(condition == 1,2),'ro','MarkerFaceColor','r')
-% plot(x,turbfit,'b',x,lamfit,'r',x,allfit,'k')
-plot(x,allfit,'k')
-xlabel('Pectoral Fin Beat Frequency (Hz)','FontSize',12); ylabel('Oxygen Consumption Rate (VO_2 ; mg O_2 kg^-^1 h^-^1)','FontSize',12)
-
+%%
